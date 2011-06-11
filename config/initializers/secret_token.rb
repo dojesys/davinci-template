dd8fb4af1eb4521116f4cbf9c3ef31e392a049a8
Johanan Pacheco <johananp@gmail.com>
2011-06-10 20:58:30 -0430

    Carga inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DavinciTemplate::Application.config.secret_token = 'c7a6677a04bfd473e0fbf1f93ce9df6e96c985307b4fa4dcfb9671d62eae34b11fa4678de012449b9d7218aef3602e7bb923f1b5a431b271adab111f79dd423d'
